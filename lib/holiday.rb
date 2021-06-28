48ef14febc6103c6e38e53adb50a517895b1ba26
maxrabin <max.rabin17@gmail.com>
2021-06-28 11:28:59 -0400

    commit tree f6063f18979d29e96a1b98457fdb4861064c28ed

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-      puts "  #{holiday.to_s.split('_').map {|word| word.capitalize}.join(' ')}: #{supplies.join(", ")}"
+      puts "#{holiday.to_s.split('_').map {|word| word.capitalize}.join(' ')}: #{supplies.join(", ")}"
     end
   end
 end
